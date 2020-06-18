cd2984a554194c892b444eab56483d084f7867c0
luzufeng <luzufeng@cqpwrd.com>
2020-06-18 16:49:44 +0800

    merge

--- app/src/main/java/com/lzf/wanandroidapp/ui/KtActivity.kt
+++ app/src/main/java/com/lzf/wanandroidapp/ui/KtActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.lzf.wanandroidapp.R
 import kotlinx.android.synthetic.main.activity_kt.*
 
@@ -15,24 +16,8 @@ class KtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kt)
-        kotlin_test_button.run {
-            setOnClickListener {
-                text = "收到点击事件"
-                Log.d(thisFile, "")
-            }
-        }
-        Glide.with(this).asGif();
-        when(thisFile){
-            "1"->{ }
-            "2"->{ }
-            else -> {
-                Log.d(thisFile, "啥也不是")
 
-            }
-        }
+        //Glide.with(this).applyDefaultRequestOptions(RequestOptions.circleCropTransform()).load(R.mipmap.ic_launcher).into(role);
     }
 
-    fun Test() {
-
-    }
 }
